36d62ce1f5f255664df66c108be203fa93212fa8
Aroun Rdj <aroun.rdj@ik.me>
2025-09-02 10:20:19 +0200

    add a url ref and bind it to an anchor tag's href attribute

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup>
 import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+import qrCode from './assets/images/qrcode.png'
 
 const product = ref('Socks')
-const image = ref(socksGreenImage)
+const image = ref(qrCode)
+const website = ref('https://main.d3fxshzbjh5gns.amplifyapp.com/')
 
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+         <a v-bind:href="website"><img v-bind:src="image"></a>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
